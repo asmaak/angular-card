94a2ac6f688351da0eeee3edb47b0e3e31bba1e8
asmaa kamal <asma2_kamal2009@yahoo.com>
2020-05-08 06:56:05 -0400

    angular card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imgUrl: 'assets/tree.jpeg',
+      userName: 'natural',
+      content: 'I saw tree awesom dyring my hike today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imgUrl: 'assets/mountain.jpeg',
+      userName: 'mountainLover',
+      content: 'Here is a picture of snowy mountain'
+    },
+    {
+      title: 'Mountain Bicking',
+      imgUrl: 'assets/biking.jpeg',
+      userName: 'biking1222',
+      content: 'I did some biking today'
+    }
+
+  ];
 }
